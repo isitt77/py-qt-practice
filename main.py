bf07f72a1b0f9937059157cb5b0215c555206d9f
Scott Isitt <isitts@hotmail.com>
2022-08-21 01:06:34 -0700

    Update pip and install PySide6.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QMainWindow
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
+    win.setWindowTitle("PyQt Window")
     # ...
+    win.show()
     sys.exit(app.exec())
