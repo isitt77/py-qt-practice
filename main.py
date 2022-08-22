7c0dfdb9d25ed0ea50187d333cb3b3df1f6510f0
Scott Isitt <isitts@hotmail.com>
2022-08-21 23:26:07 -0700

    Add button and connect it with clicked().

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from PySide6 import QtWidgets
 from PySide6.QtWidgets import QApplication, QMainWindow
 
 
+def clicked():
+    print("Clicked!")
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     win = QMainWindow()
@@ -15,6 +18,11 @@ if __name__ == "__main__":
     label.setText("Label")
     # label positioning
     label.move(140,50)
+
+    #Buttons
+    b1 = QtWidgets.QPushButton(win)
+    b1.setText("Submit")
+    b1.clicked.connect(clicked)
 
     win.show()
     sys.exit(app.exec())
