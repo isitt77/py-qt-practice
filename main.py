a95fe505013b721dd18b369a7dd45b1d4b3d7089
Scott Isitt <isitts@hotmail.com>
2022-08-21 23:06:42 -0700

    Add label to window.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
 import sys
+from PySide6 import QtWidgets
 from PySide6.QtWidgets import QApplication, QMainWindow
 
 
@@ -8,6 +9,12 @@ if __name__ == "__main__":
     win = QMainWindow()
     win.setGeometry(200, 200, 300, 300)
     win.setWindowTitle("PyQt Window")
-    # ...
+
+    # location of label
+    label = QtWidgets.QLabel(win)
+    label.setText("Label")
+    # label positioning
+    label.move(140,50)
+
     win.show()
     sys.exit(app.exec())
